98eb0b8ea7b4c6ae4e13a8dfc6c73c07aa620890
Brian Silverman <brians@playfulinvention.com>
2018-02-21 17:40:21 -0500

    fixed gitignore

--- Logo/bitlogo-vm/build/bbc-microbit-classic-gcc/ym/ble/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/brian/files/projects/microbitlogo/bitlogo-vm/build/bbc-microbit-classic-gcc/ym/ble
-# Build directory: /Users/brian/files/projects/microbitlogo/bitlogo-vm/build/bbc-microbit-classic-gcc/ym/ble
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(source)
--- Logo/bitlogo-vm/build/bbc-microbit-classic-gcc/ym/microbit/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/brian/files/projects/microbitlogo/bitlogo-vm/build/bbc-microbit-classic-gcc/ym/microbit
-# Build directory: /Users/brian/files/projects/microbitlogo/bitlogo-vm/build/bbc-microbit-classic-gcc/ym/microbit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(../microbit-dal)
-subdirs(source)
